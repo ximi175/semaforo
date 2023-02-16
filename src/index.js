47ff3aaf39e6c14b6ad9d808db1614a86f0fd7f1
Ximo Serra <chimitin82@gmail.com>
2023-02-16 14:44:27 +0100

    fix border semaforo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 //diseñar semaforo visualmente que empiece en verde y que a los 10 segundos cambie a ambar y a los 2 segundos y del ambar a rojo a los 10 segundos vuelva a empezar;
 //mientras el verde este activo el resto en negro
-const ambar = document.getElementById('ambar')
-const rojo = document.getElementById('rojo')
-const verde = document.getElementById('verde')
-
+const ambar = document.getElementById('ambar');
+const rojo = document.getElementById('rojo');
+const verde = document.getElementById('verde');
+const semaforo = document.getElementById('semaforo');
 
 function cambiarColores() {
     ambar.style.backgroundColor = 'black';
@@ -11,16 +11,18 @@ function cambiarColores() {
     verde.style.backgroundColor = 'green';
     verde.style.border = '3px solid rgb(26 180 26)';
     rojo.style.border = '3px solid rgb(41 38 38)';
+    semaforo.style.borderColor = 'green';
 setTimeout(() => {
   verde.style.backgroundColor = 'black';
   ambar.style.backgroundColor = 'orange';
   rojo.style.backgroundColor = 'black';
   ambar.style.border = '3px solid #f37500';
   verde.style.border = '3px solid rgb(41 38 38)';
+  semaforo.style.borderColor = 'orange';
 
 
 }, 10000);
-setTimeout(() => {
+/* setTimeout(() => {
   rojo.style.backgroundColor = 'red';
   ambar.style.backgroundColor = 'orange';
   verde.style.backgroundColor = 'black';
@@ -28,13 +30,14 @@ setTimeout(() => {
   rojo.style.border = '3px solid #f30071';
 
   ambar.style.border = '3px solid rgb(41 38 38)';
-}, 13500);
+}, 13500); */
 setTimeout(() => {
   rojo.style.backgroundColor = 'red';
   ambar.style.backgroundColor = 'black';
   verde.style.backgroundColor = 'black';
   rojo.style.border = '3px solid #f30071';
   ambar.style.border = '3px solid rgb(41 38 38)';
+  semaforo.style.borderColor = 'red';
 }, 15000);
 setInterval(() => {
   cambiarColores();
